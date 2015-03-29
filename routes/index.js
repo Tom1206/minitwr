851663ddd8aadd597a710f0aee6e155057a46a24
Théo Poizat <theo.poizat@etu.unistra.fr>
2015-03-29 15:06:08 +0200

    Add registration with MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var router = express.Router();
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  nickname: String,
+  mail: String,
+  password: String
+});
+
+var userModel = mongoose.model('User', userSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res) {
+  console.log('name ' + req.body.Nickname);
+  console.log('name ' + req.body.Mail);
+  console.log('name ' + req.body.Password);
+  var newuser = new userModel({ nickname : req.body.Nickname, mail : req.body.Mail, password : req.body.Password});
+  newuser.save();
 });
 
 module.exports = router;
