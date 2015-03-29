3a13c227f542c3ef966a5c5d2cb8e13254d466a7
Théo Poizat <theo.poizat@etu.unistra.fr>
2015-03-29 12:24:20 +0200

    Add about.jade route

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
-// view engine setup
+// view engtine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/about', about);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
