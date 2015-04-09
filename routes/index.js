74a60b92b4cda257d9fd6ca0207a0b306569a0cf
Nicolas <nsutter@etu.unistra.fr>
2015-04-09 19:46:55 +0200

    search bar use now the regular expression

--- routes/index.js
+++ routes/index.js
@@ -73,8 +73,8 @@ module.exports = function(passport){
 /* search tool */
 
 	router.post('/search', isAuthenticated, function(req, res) {
-		tweet.find({tweet: req.body.research}, function(err,tweets) {
-			User.find({username: req.body.research}, function(err,users) {
+		tweet.find({tweet: new RegExp(req.body.research, 'i')}, function(err,tweets) {
+			User.find({username: new RegExp(req.body.research, 'i')}, function(err,users) {
 				res.render('search', {user: req.user, tweets: tweets, users: users});
 			});
 		});
