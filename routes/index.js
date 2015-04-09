130a07e47281eeef480eb78b6cc0651ab72d15da
Nicolas <nsutter@etu.unistra.fr>
2015-04-09 19:58:01 +0200

    search results are now sort by date for the tweet

--- routes/index.js
+++ routes/index.js
@@ -73,7 +73,7 @@ module.exports = function(passport){
 /* search tool */
 
 	router.post('/search', isAuthenticated, function(req, res) {
-		tweet.find({tweet: new RegExp(req.body.research, 'i')}, function(err,tweets) {
+		tweet.find({tweet: new RegExp(req.body.research, 'i')}).sort({date: -1}).exec(function(err,tweets) {
 			User.find({username: new RegExp(req.body.research, 'i')}, function(err,users) {
 				res.render('search', {user: req.user, tweets: tweets, users: users});
 			});
@@ -92,7 +92,7 @@ module.exports = function(passport){
 	});
 
 	router.get('/publicprofile', isAuthenticated, function(req, res){
-				tweet.find({id: req.user._id}).limit(10).sort({date: -1}).exec( function (err, tweets) {
+				tweet.find({id: req.user._id}).limit(10).sort({date: -1}).exec(function (err, tweets) {
 						if (err) return console.error(err);
 						res.render('publicprofile', { user: req.user, tweet: tweets});
 				});
