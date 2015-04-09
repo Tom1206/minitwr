4530f4ff13cdd62e9b0b138429c2d23bcb81c0a3
Nicolas <nsutter@etu.unistra.fr>
2015-04-09 13:55:41 +0200

    add the tweet to the public profile

--- routes/index.js
+++ routes/index.js
@@ -82,7 +82,6 @@ module.exports = function(passport){
 	});
 
 	/* /profile */
->>>>>>> a835012794ed6c58a69caaee6d8b237032e1189f
 
 	router.get('/profile', isAuthenticated, function(req, res){
 				res.render('profile', { user: req.user, name_picture: req.user.picture});
@@ -94,9 +93,12 @@ module.exports = function(passport){
 	});
 
 	router.get('/publicprofile', isAuthenticated, function(req, res){
-				res.render('publicprofile', { user: req.user});
+				tweet.find({id: req.user._id}).limit(10).sort({date: -1}).exec( function (err, tweets) {
+						if (err) return console.error(err);
+						res.render('publicprofile', { user: req.user, tweet: tweets});
+				});
 			});
-			
+
 	// upload profile picture
 	router.post('/upload', isAuthenticated, function(req, res) {
 		var form = new formidable.IncomingForm();
