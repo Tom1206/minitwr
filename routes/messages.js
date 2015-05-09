2b7c172eeb5e0c47d8187987f60d41db7224759d
Nicolas <nsutter@etu.unistra.fr>
2015-05-09 12:37:39 +0200

    graphical update

--- routes/messages.js
+++ routes/messages.js
@@ -33,13 +33,12 @@ var server = require('http').createServer(router),
 
 		// Quand on client se connecte
     io.sockets.on('connection', function (socket, pseudo) {
-      
+
 
       socket.on('message', function (mess, pseudo) {
           console.log('message : ' + mess + ' de ' + pseudo);
           socket.broadcast.emit('message', {pseudo: pseudo, message: mess});
       });
-
 
     });
 
