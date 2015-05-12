a6d218fceef492d06724cc4bcbe56a126817e88d
Théo Poizat <theo.poizat@zatteo.com>
2015-05-12 15:01:35 +0200

    New layout.jade

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,14 @@ function addLocation(){
     long = position.coords.longitude;
     form.latitude.value = lat;
     form.longitude.value = long;
+
+    $.noty.defaults.killer = true;
+      noty({
+        text: 'Congratulations ! Your location has been successfully loaded !',
+        layout: 'bottom',
+        closeWith: ['click', 'hover'],
+        type: 'success'
+        });
   }
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(getPos);
