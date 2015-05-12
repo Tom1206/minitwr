87d1fe2f7bdc927b0d679b1e598e047815be0c53
Théo Poizat <theo.poizat@zatteo.com>
2015-05-12 15:38:11 +0200

    Useless old module deleted and fix css location issue in publicprofile.jade

--- passport/signup.js
+++ passport/signup.js
@@ -12,7 +12,7 @@ module.exports = function(passport){
             findOrCreateUser = function(){
                 // find a user in Mongo with provided username
                 User.findOne({ 'username' :  username }, function(err, user) {
-                    // In case of any error, return using the done method
+                    // in case of any error, return using the done method
                     if (err){
                         console.log('Error in SignUp: '+err);
                         return done(err);
@@ -34,22 +34,22 @@ module.exports = function(passport){
                         // save the user
                         newUser.save(function(err) {
                             if (err){
-                                console.log('Error in Saving user: '+err);
+                                console.log('Error in saving user: '+err);
                                 throw err;
                             }
-                            console.log('User Registration succesful');
+                            console.log('User registration succesful');
                             return done(null, newUser);
                         });
                     }
                 });
             };
-            // Delay the execution of findOrCreateUser and execute the method
+            // delay the execution of findOrCreateUser and execute the method
             // in the next tick of the event loop
             process.nextTick(findOrCreateUser);
         })
     );
 
-    // Generates hash using bCrypt
+    // generates hash using bCrypt
     var createHash = function(password){
         return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
     }
